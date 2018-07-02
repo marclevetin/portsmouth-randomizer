3d04803e4e0da3b3dade8e382c1bd361154b6737
marclevetin <marc@derbyhat.org>
2018-07-02 19:34:37 -0400

    added toggle for displaying Group Lead attribute

--- react/src/components/Group.js
+++ react/src/components/Group.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const Group = props => {
+  const displayGroupLead = (props.displayGroupLead) ? "(lead)" : ""
   return(
     <div className="group">
       <h3>Group {props.index}</h3>
       <p>
-        {props.lead} (lead), {props.participants.join(', ')}
+        {props.lead} {displayGroupLead}, {props.participants.join(', ')}
       </p>
     </div>
   )
